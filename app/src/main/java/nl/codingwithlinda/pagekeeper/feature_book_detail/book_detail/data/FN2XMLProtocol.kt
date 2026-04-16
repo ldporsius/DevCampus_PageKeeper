998ad67e993c644c3aad3423977e12c21afbc551
ldporsius <linda.d.porsius@gmail.com>
2026-04-16 08:14:29 +0200

    fn2 protocol tags

--- app/src/main/java/nl/codingwithlinda/pagekeeper/feature_book_detail/book_detail/data/FN2XMLProtocol.kt
+++ app/src/main/java/nl/codingwithlinda/pagekeeper/feature_book_detail/book_detail/data/FN2XMLProtocol.kt
@@ -3,3 +3,5 @@ package nl.codingwithlinda.pagekeeper.feature_book_detail.book_detail.data
 val bodyTag = "<body[^>]*?>(.*?)</body>"
 val sectionTag = "<section[^>]*?>(.*?)</section>"
 val paragraphTag = "<p[^>]*?>(.*?)</p>"
+val citeTage = "<cite[^>]*?>(.*?)</cite>"
+val epigraphTag = "<epigraph[^>]*?>(.*?)</epigraph>"
